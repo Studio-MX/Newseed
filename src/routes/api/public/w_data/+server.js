0c15b1e5b99c5fa34fad675250809688413ba394
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-09 20:15:45 +0900

    Update +server.js

--- src/routes/api/public/w_data/+server.js
+++ src/routes/api/public/w_data/+server.js
@@ -1,4 +1,4 @@
 export async function GET ({ fetch, request }) {
   let data = '== 개요 ==\n아 존나 귀찮다';
-  return new Response(JSON.stringify(data));
+  return new Response(JSON.stringify({content: {data}}));
 };
